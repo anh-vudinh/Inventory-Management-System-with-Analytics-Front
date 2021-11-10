e0f83a45913e2ac2b95f237b65d64d92bf0ea780
vu <anhuvdinh@gmail.com>
2021-11-10 04:32:02 -0600

    added a placeholder for financial snapshot data for CS cards

--- src/components/company_select_page/CompanySelectCard.js
+++ src/components/company_select_page/CompanySelectCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function CompanySelectCard({company}){
-    const {name, image, location, description, is_parent, company_structure, company_type, company_model} = company
+    const {name, image, location, description, is_parent, company_structure, company_type, company_model, employees, children} = company
     
     const genericLogo = "https://istvankocsis.gallerycdn.vsassets.io/extensions/istvankocsis/reactcodesnippets/3.1/1582843313592/Microsoft.VisualStudio.Services.Icons.Default"
     const objectAssociations = [
@@ -17,7 +17,25 @@ function CompanySelectCard({company}){
     const companyType = objectAssociations.find(object => Object.keys(object)[0] === company_type)
     const companyStructure = objectAssociations.find(object => Object.keys(object)[0] === company_structure)
 
-    const companyDetailsArray = ["Location", "Industry", "Employees", "Children"]
+    const companyDetailsNameArrayA = ["Industry", "Location"]
+    const companyDetailsValueArrayA = [company_model, location]
+
+    const companyDetailsA = companyDetailsNameArrayA.map((detail, index) => 
+        <div className="CompanySelectDetailsSubText" key={detail}>
+            <p>{`${detail} :`}</p>
+            <p>{companyDetailsValueArrayA[index]}</p>
+        </div>
+    )
+
+    const companyDetailsNameArrayB = ["Employees", "Children"]
+    const companyDetailsValueArrayB = [employees, children]
+
+    const companyDetailsB = companyDetailsNameArrayB.map((detail, index) => 
+        <div className="CompanySelectDetailsSubText" key={detail}>
+            <p>{`${detail} :`}</p>
+            <p>{companyDetailsValueArrayB[index]}</p>
+        </div>
+    )
 
     return(
         <div className="CompanySelectCard">
@@ -31,14 +49,29 @@ function CompanySelectCard({company}){
             </div>
 
             <div className="CompanySelectSnapshot">
-                <p className="CompanySelectSnapshotTitle"> Snapshot</p>
-
+                <div className="CompanySelectSnapshotTitle"> 
+                    <p> $1,000,000 (placeholder)</p>
+                </div>
+                <div className="CompanySelectSnapshotAsset">
+                    <p>Assets:</p>
+                </div>
+                <div className="CompanySelectSnapshotLiability">
+                    <p>Liabilities:</p>
+                </div>
+                <div className="CompanySelectSnapshotEquity">
+                    <p>Equity:</p>
+                </div>
             </div>
             <div className="CompanySelectDetails">
-                <div className="CompanySelectDetailsText">
-                    <p>{`Location: ${location}`}</p>
-                    <p>{`Industry: ${company_model}`}</p>
+                <div className="CompanySelectDetailsTextContainer">
+                    <div className="CompanySelectDetailsTextA">
+                        {companyDetailsA}
+                    </div>
+                    <div className="CompanySelectDetailsTextB">
+                        {companyDetailsB}
+                    </div>
                 </div>
+
                 <div className="CompanySelectDetailsIcons">
                     <img src={Object.values(companyType)[0]} alt={company_type} title={company_type}/>
                     <p title={company_structure}>{Object.values(companyStructure)[0]}</p>
